9db613608f207376ba88e3a936c2e421a18ae5b6
Justin Reardon <justin.reardon@gmail.com>
2019-02-10 12:11:16 -0500

    implement deleting items

--- src/Components/InitRow.tsx
+++ src/Components/InitRow.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { TableRow, TableCell, withStyles } from "@material-ui/core";
+import { TableRow, TableCell, withStyles, IconButton } from "@material-ui/core";
 import { InitiativeEntry } from "../Model/InitiativeEntries";
 import * as InitModel from "../Model/InitModel";
+import { Delete } from "@material-ui/icons";
 
 const styles = (theme: any) => ({
     initCell: {
         width: '3em',
         align: 'right',
         padding: 'checkbox'
+    },
+    actionCell: {
+        width: '3em'
     }
 });
 
@@ -26,6 +30,11 @@ function InitRow(props: { classes: any, item: InitiativeEntry }) {
         <TableRow selected={item.active}>
             <TableCell className={classes.initCell}>{item.init}</TableCell>
             <TableCell>{item.name}</TableCell>
+            <TableCell className={classes.actionCell}>
+                <IconButton onClick={e => InitModel.removeInitEntry(item)}>
+                    <Delete />
+                </IconButton>
+            </TableCell>
         </TableRow>
     );
 }
--- src/Model/InitModel.ts
+++ src/Model/InitModel.ts
@@ -13,6 +13,7 @@ export {
     startCombat,
     nextInit,
     addInitEntry,
+    removeInitEntry,
     getInitItems,
     subscribeToInitEntries,
     unsubscribeToInitEntries,
@@ -23,10 +24,13 @@ export {
 /* Combat State */
 
 let combatState = NotStarted;
-const {fire: fireCombatStateEvents, subscribe: subscribeToCombatState, unsubscribe: unsubscribeToCombatState} =
+const { fire: fireCombatStateEvents, subscribe: subscribeToCombatState, unsubscribe: unsubscribeToCombatState } =
     createEventHandler(() => combatState);
 
 function startCombat() {
+    if (initiativeItems.length == 0) {
+        return;
+    }
     combatState = new CurrentCombatState(1, 0);
     const firstEntry = initIndexToEntry(0);
     firstEntry.entry = firstEntry.entry.toggleActive();
@@ -40,7 +44,7 @@ let nextInitId = 0;
 const initiativeItems: number[] = [];
 const initiativeItemLookup:
     { [id: number]: InitiativeInfo } = {};
-const {fire: fireInitEntryEvents, subscribe: subscribeToInitEntries, unsubscribe: unsubscribeToInitEntries} =
+const { fire: fireInitEntryEvents, subscribe: subscribeToInitEntries, unsubscribe: unsubscribeToInitEntries } =
     createEventHandler(() => getInitItems);
 
 function subscribeToInitEntry(id: number, handler: Handler<InitiativeEntry>) {
@@ -48,7 +52,8 @@ function subscribeToInitEntry(id: number, handler: Handler<InitiativeEntry>) {
 }
 
 function unsubscribeToInitEntry(id: number, handler: Handler<InitiativeEntry>) {
-    return initiativeItemLookup[id].handlers.unsubscribe(handler);
+    const entry = initiativeItemLookup[id]
+    return entry && entry.handlers.unsubscribe(handler);
 }
 
 function getInitItems(): InitiativeEntry[] {
@@ -85,6 +90,34 @@ function addInitEntry(name: string, init: number) {
     fireInitEntryEvents();
 }
 
+function removeInitEntry(entry: InitiativeEntry) {
+    const index = initiativeItems.findIndex(x => x == entry.id);
+    if (index == -1) {
+        throw new Error("Tried to remove initiative entry that doesn't exist. ID:" + entry.id);
+    }
+    initiativeItems.splice(index, 1);
+    delete initiativeItemLookup[entry.id];
+
+    if (combatState instanceof CurrentCombatState) {
+        if (initiativeItems.length == 0) {
+            combatState = NotStarted;
+        } else if (index >= combatState.activeItem) {
+            // The active index has to be updated.
+            const newActiveIndex = Math.max(0, combatState.activeItem - 1);
+            combatState = new CurrentCombatState(combatState.round, newActiveIndex);
+
+            // And the actual active item if we removed the current selection.
+            if (entry.active) {
+                const newActiveItem = initIndexToEntry(newActiveIndex);
+                toggleActive(newActiveItem)
+                newActiveItem.handlers.fire();
+            }
+        }
+        fireCombatStateEvents();
+    }
+    fireInitEntryEvents();
+}
+
 function nextInit() {
     if (!(combatState instanceof CurrentCombatState)) {
         return;
@@ -93,10 +126,14 @@ function nextInit() {
     combatState = combatState.next(initiativeItems.length);
     const nextItem = initiativeItemLookup[initiativeItems[combatState.activeItem]];
 
-    initiativeItemLookup[lastItem.entry.id].entry = lastItem.entry.toggleActive();
-    initiativeItemLookup[nextItem.entry.id].entry = nextItem.entry.toggleActive();
+    toggleActive(lastItem);
+    toggleActive(nextItem);
 
     fireCombatStateEvents();
     lastItem.handlers.fire();
     nextItem.handlers.fire();
+}
+
+function toggleActive(info: InitiativeInfo) {
+    initiativeItemLookup[info.entry.id].entry = info.entry.toggleActive();
 }
