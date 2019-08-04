bd6ef1013dd531d1730113837037df97fdc8976f
Justin Reardon <justin.reardon@gmail.com>
2019-08-03 20:42:05 -0400

    Support resetting init state.

--- src/Components/NextInitButton.tsx
+++ src/Components/NextInitButton.tsx
@@ -2,29 +2,42 @@ import React from "react";
 import * as InitModel from "../Model/InitModel";
 import { CurrentCombatState } from "../Model/CombatState";
 import { withStyles, Fab } from "@material-ui/core";
-import { PlayArrowOutlined } from "@material-ui/icons";
+import { PlayArrowOutlined, DeleteSweep } from "@material-ui/icons";
 
 const styles = (theme: any) => ({
-    fab: {
-        color: 'primary' as 'primary',
+    fabBox: {
         position: 'absolute' as 'absolute',
         bottom: theme.spacing(2),
-        right: theme.spacing(2),
+        right: theme.spacing(2)
+    },
+    fab: {
+        marginLeft: theme.spacing(2)
       }
   });
 
-function NextInitButton(props: any) {
+function NextInitButton(props: any) { 
     const { classes } = props;
     const combatState = InitModel.useCombatStateEvents(() => {});
+    const initItems = InitModel.useInitEntryListEvents(() => {});
+    const noInitItems = initItems.length === 0;
     
     return (
-        <Fab className={classes.fab} 
-             color='primary'
-             onClick={() => combatState instanceof CurrentCombatState ? 
-                                InitModel.nextInit() : 
-                                InitModel.startCombat()}>
-            <PlayArrowOutlined />
-        </Fab>
+        <div className={classes.fabBox}>
+            <Fab className={classes.fab}
+                color='secondary'
+                disabled={noInitItems}
+                onClick={() => initItems.forEach(x => InitModel.removeInitEntry(x))}>
+                <DeleteSweep />
+            </Fab>
+            <Fab className={classes.fab} 
+                color='primary'
+                disabled={noInitItems}
+                onClick={() => combatState instanceof CurrentCombatState ? 
+                                    InitModel.nextInit() : 
+                                    InitModel.startCombat()}>
+                <PlayArrowOutlined />
+            </Fab>
+        </div>
     );
     
 
