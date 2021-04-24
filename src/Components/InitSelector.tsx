6ff3e689ab3815b2e0e1cefffc49a6a439f3bee5
Justin Reardon <justin.reardon@gmail.com>
2021-04-24 11:08:22 -0400

    Use a grid to select initiative

--- src/Components/InitSelector.tsx
+++ src/Components/InitSelector.tsx
@@ -1,61 +1,70 @@
 import React, { useState } from 'react';
 import { PlaylistAdd } from '@material-ui/icons';
-import { withStyles, IconButton, Menu, MenuItem } from '@material-ui/core';
+import { withStyles, IconButton, Popover, Button, GridList } from '@material-ui/core';
 type MouseEvt = React.MouseEvent<HTMLElement, MouseEvent>;
 
 const styles = (theme: any) => ({
-    button: {
-      margin: theme.spacing(1),
-    }
+  button: {
+    margin: theme.spacing(1),
+  },
+  popupGrid: {
+    width: 64 * 5
+  },
 });
 
-const inits = [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2];
+const inits = Array.from({ length: 25 }, (_, i) => i - 1);
 
-function InitSelector(props: {classes: any, disabled: boolean, onSelected: ((x: number) => void)}) {
-    const { classes, disabled, onSelected } = props;
+function InitSelector(props: { classes: any, disabled: boolean, onSelected: ((x: number) => void) }) {
+  const { classes, disabled, onSelected } = props;
 
-    const [anchor, setAnchor] = useState<HTMLElement|null>(null);
+  const [anchor, setAnchor] = useState<HTMLElement | null>(null);
 
-    const handleClickButton = (event: MouseEvt) => {
-        setAnchor(event.currentTarget);
-    };
-  
-    const handleMenuItemClick = (event: MouseEvt, index: number) => {
-        setAnchor(null);
-        onSelected(inits[index]);
-    };
-  
-    const handleClose = () => {
-        setAnchor(null);
-    };
+  const handleClickButton = (event: MouseEvt) => {
+    setAnchor(event.currentTarget);
+  };
 
-    return (
-        <span>
-            <IconButton 
-                onClick={handleClickButton}
-                className={classes.button} 
-                disabled={disabled}
-                aria-label="Add to Initiative"
-            >
-                <PlaylistAdd/>
-            </IconButton>
-            <Menu                
-                id="lock-menu"
-                anchorEl={anchor}
-                open={Boolean(anchor)}
-                onClose={handleClose}
-            >
-            {inits.map((option, index) => (
-                <MenuItem
-                    dense={true}
-                    key={option}
-                    onClick={event => handleMenuItemClick(event, index)}
-                >
-                {option}
-                </MenuItem>
-            ))}
-            </Menu>
-        </span>)
+  const handleMenuItemClick = (event: MouseEvt, index: number) => {
+    setAnchor(null);
+    onSelected(inits[index]);
+  };
+
+  const handleClose = () => {
+    setAnchor(null);
+  };
+
+  return (
+    <span>
+      <IconButton
+        onClick={handleClickButton}
+        className={classes.button}
+        disabled={disabled}
+        aria-label="Add to Initiative"
+      >
+        <PlaylistAdd />
+      </IconButton>
+      <Popover
+        id="lock-menu"
+        anchorEl={anchor}
+        open={Boolean(anchor)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+      >
+        <GridList cols={5} cellHeight={48} spacing={0} className={classes.popupGrid}>
+          {inits.map((option, index) => (
+            <Button size='small' onClick={event => handleMenuItemClick(event, index)}>
+              {option}
+            </Button>
+          ))}
+        </GridList>
+      </Popover>
+    </span>)
 }
 
 export default withStyles(styles)(InitSelector);
